993f6dc623bbf8979ea121b0b79396b204d74049
Ethan Doering <ethanwdoering@icloud.com>
2023-02-17 10:46:34 -0700

    Created the __init()__ and the base functions for every class

---
+++ dinosaur.py
@@ -0,0 +1,10 @@
+from robot import Robot
+
+class Dinosaur:
+    def __init__(self):
+        self.name = "Bob"
+        self.attack_power = 15
+        self.health = 50
+
+    def attack(self, Robot):
+        pass
---
+++ robot.py
@@ -0,0 +1,11 @@
+from weapon import Weapon
+
+class Robot:
+
+    def __init__(self):
+        self.name = "Gerald"
+        self.health = 100
+        self.active_weapon = Weapon
+
+    def attack(self, dinasour):
+        pass
